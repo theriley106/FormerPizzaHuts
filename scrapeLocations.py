15c038e03358ce980b5c4a8964d18c7667df21f0
Theriley106 <christopherlambert106@gmail.com>
2018-06-18 21:19:09 -0400

    Added proxy

--- scrapeLocations.py
+++ scrapeLocations.py
@@ -12,8 +12,8 @@ formerPizzaHuts = []
 
 THREADS = 10
 with open('locations.csv', 'rb') as f:
-    reader = csv.reader(f)
-    your_list = list(reader)
+	reader = csv.reader(f)
+	your_list = list(reader)
 
 ADDRESS_LIST = []
 i = 0
@@ -31,8 +31,10 @@ addressList = chunks(ADDRESS_LIST, int(len(ADDRESS_LIST)/THREADS))
 def grabSite(url):
 	for i in range(3):
 		try:
+			proxy = "108.59.14.203:13010"
+			proxies = {"http": proxy, "https": proxy}
 			headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-			return requests.get(url, headers=RandomHeaders.LoadHeader(), timeout=10)
+			return requests.get(url, headers=RandomHeaders.LoadHeader(), proxies=proxies, timeout=10)
 		except Exception as exp:
 			print exp
 			pass
@@ -81,11 +83,13 @@ def processLocations(listOfLocations):
 
 if __name__ == '__main__':
 	threads = [threading.Thread(target=processLocations, args=(ar,)) for ar in addressList]
-
 	for thread in threads:
 		thread.start()
 	for thread in threads:
 		thread.join()
+	with open ("test.txt","w")as fp:
+		for line in formerPizzaHuts:
+			fp.write(line+"\n")
 
 
 
